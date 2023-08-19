945e355af79b5c811fbb94bc6c4f5ad892d6ef19
hms <1805236878@qq.com>
2023-08-19 15:55:47 +0800

    Log Aspect

--- src/main/java/com/hms/livefreedom/application/service/GetUserAppService.java
+++ src/main/java/com/hms/livefreedom/application/service/GetUserAppService.java
@@ -1,24 +1,29 @@
 package com.hms.livefreedom.application.service;
 
 import com.hms.livefreedom.domain.entity.SysUser;
-import com.hms.livefreedom.service.SysLogService;
+import com.hms.livefreedom.infrastructure.annotation.Log;
 import com.hms.livefreedom.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetUserAppService {
 
     @Autowired
-    private SysLogService logService;
-
-    @Autowired
     private SysUserService userService;
 
+    @Log("查询用户")
     @GetMapping("/getUser")
-    public SysUser index() {
-        System.out.println(userService.getById(1));
+    public SysUser getUserById() {
         return userService.getById(1);
+    }
+
+    @Log("新增用户")
+    @PostMapping("/addUser")
+    public boolean addUser(@RequestBody SysUser user) {
+        return userService.save(user);
     }
 }
